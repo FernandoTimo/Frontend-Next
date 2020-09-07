952f49ead1b9e3a9d565e93163ca05df1e8ae3c5
FernandoTimo <Fertimo99@gmail.com>
2020-09-07 14:10:48 -0500

    useSockets documentación

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import styleIndex from 'styles/css/Index.module.css';
 import { GlobalHead } from 'heads/GlobalHead';
 import { Body, Section } from 'components/Timoideas';
+import { useSockets } from 'hooks/useSockets';
+import { useEffect } from 'react';
 export default function Index() {
+  useEffect();
+  useSockets();
   return (
     <>
       <GlobalHead />
