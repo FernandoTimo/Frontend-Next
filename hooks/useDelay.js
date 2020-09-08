de4e7ebe52e1c78d352beefedc8ebe935c381b47
FernandoTimo <Fertimo99@gmail.com>
2020-09-08 15:33:19 -0500

    useDelay

--- hooks/useDelay.js
+++ hooks/useDelay.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react';
-const [act, setact] = useState(false);
-useEffect(() => {
-  setTimeout(() => {
-    setact(true);
-  }, 5000);
-});
+
+export default function useDelay(delay = 1000) {
+  const [Active, setActive] = useState(false);
+  useEffect(() => {
+    const retraso = setTimeout(() => {
+      setActive(true);
+    }, delay);
+    return () => {
+      clearTimeout(retraso);
+    };
+  }, [Active]);
+
+  return Active;
+}
