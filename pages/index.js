6ebd30f46c7da0c3529962cf8d0dfb7b019cfe56
FernandoTimo <Fertimo99@gmail.com>
2020-09-08 16:09:44 -0500

    useDelay uso interesante

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,20 @@ import { useEffect, useState } from 'react';
 import useDelay from 'hooks/useDelay';
 
 export default function Index() {
-  const [State, setState] = useState('Hola');
-
+  const [State, setState] = useState('Fernando Timo');
+  useDelay(() => {
+    setState('es');
+  }, 1000);
+  useDelay(() => {
+    setState('Timoideas');
+  }, 2000);
   return (
     <>
       <GlobalHead />
       <Body>
-        <Section size={1}>{State}</Section>
+        <Section size={1}>
+          <h1>{State}</h1>
+        </Section>
       </Body>
     </>
   );
