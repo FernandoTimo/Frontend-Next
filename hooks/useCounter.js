d59dd800b2c4bb6204b737f6d4c3981dd5a2aa5d
FernandoTimo <Fertimo99@gmail.com>
2020-09-08 10:59:45 -0500

    useCounter Documentado

--- hooks/useCounter.js
+++ hooks/useCounter.js
@@ -1,16 +1,19 @@
 import { useState, useEffect } from 'react';
-
+/**
+ * ⚡ Hoook UseCounter ⚡ Devuelve los valores intermedios de el min-max en un intervalo de tiempo
+ * @param {number} min Valor inicial del contador
+ * @param {number} max Valor final del contador
+ * @param {number} speed Valor del intérvalo-tiempo en milisegundos
+ */
 export default function useCounter(min, max, speed) {
   const [Counter, setCounter] = useState(min);
-  // console.log(max.toString()[3]);
   useEffect(() => {
     var currentCounter;
     if (Counter < max) {
       currentCounter = setInterval(() => {
-        setCounter(Counter + 1);
+        setCounter(Counter + speed * max.toString().length);
       }, speed);
     }
-    console.log(Counter);
     return () => {
       clearInterval(currentCounter);
     };
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import { Body, Section } from 'components/Timoideas';
 import useCounter from 'hooks/useCounter';
 
 export default function Index() {
-  const numero = useCounter(5, 1545, 1);
+  const numero = useCounter(5, 1545, 9);
+  useCounter(5, 15, 15);
   return (
     <>
       <GlobalHead />
       <Body>
-        <Section size={1}>{numero}</Section>
+        <Section size={1}>
+          <h1>{numero}</h1>
+        </Section>
         {/* <Section size={1}>Hola Mundo</Section> */}
       </Body>
     </>
