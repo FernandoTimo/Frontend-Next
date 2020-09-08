149f76b208114a356e099addadd14bc04747950a
FernandoTimo <Fertimo99@gmail.com>
2020-09-08 15:20:30 -0500

    useDebounce

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,27 @@
 import styleIndex from 'styles/css/Index.module.css';
 import { GlobalHead } from 'heads/GlobalHead';
-import { Body, Section } from 'components/Timoideas';
-import useCounter from 'hooks/useCounter';
+import { Body, Section, Content } from 'components/Timoideas';
 import { useEffect, useState } from 'react';
+import useDebounce from 'hooks/useDebounce';
 
 export default function Index() {
-  const [Activ, setActive] = useState(false);
-  let Counter = useCounter(563, 80, Activ);
-
+  const [Activ, setActive] = useState('');
+  const Debounce = useDebounce(Activ);
   return (
     <>
       <GlobalHead />
       <Body>
         <Section size={1}>
-          <h1>{Counter}</h1>
-          <button
-            onClick={() => {
-              setActive(!Activ);
-            }}
-          >
-            Animar
-          </button>
+          <Content center flex={1}>
+            <input
+              type="text"
+              onChange={(e) => setActive(e.target.value)}
+              placeholder="Debounce"
+            />
+            <Content bg center flex={0.5}>
+              <h1>{Debounce}</h1>
+            </Content>
+          </Content>
         </Section>
         {/* <Section size={1}>Hola Mundo</Section> */}
       </Body>
