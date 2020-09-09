e7ca0eda6151e71f4c5adc4140a7413f2dd64eed
FernandoTimo <Fertimo99@gmail.com>
2020-09-08 19:02:43 -0500

    useDevice v1 Documentado

--- hooks/useDevice.js
+++ hooks/useDevice.js
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react';
 
+/**
+ * ⚡ Hoook useDevice ⚡ Reconoce el dispositivo y la plataforma del cliente final
+ */
 export const useDevice = () => {
-  const [device, setDevice] = useState();
+  const [Device, setDevice] = useState('');
+  const [Platform, setPlatform] = useState('');
 
   useEffect(() => {
     const handlerDevice = () => {
-      if (window.innerWidth < 600) {
-        setDevice('Phone');
+      if (window.innerWidth < 380) {
+        setPlatform('Watch');
+      } else if (window.innerWidth < 600) {
+        setPlatform('Phone');
       } else if (window.innerWidth < 768) {
-        setDevice('Tablet');
+        setPlatform('Tablet');
       } else if (window.innerWidth < 1200) {
-        setDevice('Desktop');
+        setPlatform('Laptop');
+      } else if (window.innerWidth >= 1200) {
+        setPlatform('Desktop');
       }
     };
-
+    setDevice(navigator.platform);
     handlerDevice();
-
     window.addEventListener('resize', handlerDevice);
     return () => {
       window.removeEventListener('resize', handlerDevice);
     };
   }, []);
 
-  return device;
+  return { Device, Platform };
 };
--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,22 @@ import styleIndex from 'styles/css/Index.module.css';
 import { GlobalHead } from 'heads/GlobalHead';
 import { Body, Section, Content } from 'components/Timoideas';
 import { useEffect, useState } from 'react';
-import useDelay from 'hooks/useDelay';
-import useLocalStorage from 'hooks/useLocalStorage';
-import useGeolocalization from 'hooks/useGeolocalization';
-
+import { useDevice } from 'hooks/useDevice';
 export default function Index() {
   const [State, setState] = useState('');
-  const { Country, City, Region, Coordenadas } = useGeolocalization();
+  const { Device, Platform } = useDevice();
   return (
     <>
       <GlobalHead />
       <Body>
         <Section size={1}>
+          <h1>{Device}</h1>
           <input
             type="text"
             onChange={(e) => setState(e.target.value)}
             placeholder="local"
           />
-          <h1>{Country}</h1>
-          <h1>{Region}</h1>
-          <h1>{City}</h1>
+          <h1>{Platform}</h1>
         </Section>
       </Body>
     </>
