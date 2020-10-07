fc9e885526f2bb56870005a18314da47ba9b5c2d
FernandoTimo <Fertimo99@gmail.com>
2020-10-07 11:21:57 -0500

    default title

--- heads/Header_Main.js
+++ heads/Header_Main.js
@@ -6,7 +6,7 @@ export function Header_Main() {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="mobile-web-app-capable" content="yes"></meta>
-      <title>{`Captivant | ${messages[StepStore]}`}</title>
+      <title>localhost:3000</title>
       <meta name="description" content="" />
     </Head>
   );
