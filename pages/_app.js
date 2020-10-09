6ac09a7cf77a20aca0a41370033f955a1eeb9f4b
FernandoTimo <Fertimo99@gmail.com>
2020-10-09 12:01:13 -0500

    _app

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,8 @@ import '../styles/Timoideas.css';
 import '../styles/Timoideas.min.css';
 import { ThemeContextProvider } from 'context/ThemeContext';
 import { StoreContextProvider } from 'context/StoreContext';
-
 import { Navigation } from 'components/Resources/Timoideas';
-import { useTheme } from 'hooks/useTheme';
 const App = ({ Component, pageProps }) => {
-  const { Theme } = useTheme();
   return (
     <ThemeContextProvider>
       <StoreContextProvider>
