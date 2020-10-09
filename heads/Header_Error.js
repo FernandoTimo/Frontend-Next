7ce4e3c38da2e4d2039145a935374818b305a7be
Timoideas <Timoideas@hotmail.com>
2020-10-09 13:53:18 -0500

    Error head title

--- heads/Header_Error.js
+++ heads/Header_Error.js
@@ -5,7 +5,7 @@ export function Header_Error() {
       <link rel="icon" href="/icons/favicon.ico" />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <title>Timoideas | 404</title>
+      <title>404</title>
       <meta name="description" content="Not URL found" />
     </Head>
   );
