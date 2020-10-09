1d56454a5b0fcfcb3df1e87c513a9c03ce6ce6ec
Timoideas <Timoideas@hotmail.com>
2020-10-09 13:49:08 -0500

    Error page

--- pages/404.js
+++ pages/404.js
@@ -12,26 +12,11 @@ export default function NotFound() {
       <Header_Error />
       <Section>
         <Content center flex={1} padding={0}>
-          {/* <img
-          alt="Imagen Alternativa"
-          src="https://media1.giphy.com/media/3ohc19SFUdIJ0YQcLe/giphy.gif"
-        /> */}
-          <img
-            alt="Imagen Alternativa"
-            src="https://media2.giphy.com/media/3E2Qd5tJ8Nke7E25GE/200w.webp?cid=ecf05e47a086794b623eb51547c68212e913d5deec00f8fb&rid=200w.webp"
-          />
-          <h1 className={styleError.Title} style={{ color: Theme._20 }}>
-            Error
-          </h1>
-          <h2
-            className={styleError.Path}
-            style={{ background: Theme._20, color: Theme._00 }}
-          >
-            {' '}
-            localhost:3000{asPath}
-          </h2>
+          <code className={styleError.PathCode}>
+            <path>{asPath}</path> route is not being controlled
+          </code>
           <Link href="/">
-            <a className={styleError.Back}>🍄</a>
+            <a className={styleError.Back}>Return to homepage</a>
           </Link>
         </Content>
       </Section>
