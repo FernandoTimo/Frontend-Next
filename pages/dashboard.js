a5c934f0dedc224754f3b5ebca5d991f433bd2c1
FernandoTimo <Fertimo99@gmail.com>
2021-01-25 11:50:13 -0500

    Dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,6 @@
 // import styleDashboard from 'styles/css/Dashboard.module.css';
 import { Header_Main } from 'heads/Header_Main';
 import { Section, Body, Content } from 'components/Resources/Timoideas';
-import { StoreAdmin } from 'hooks/useStore';
 export default function Dashboard() {
   return (
     <>
@@ -9,7 +8,7 @@ export default function Dashboard() {
       <Body>
         <Section size={1}>
           <Content center flex={1}>
-            <StoreAdmin />
+            Dashboard
           </Content>
         </Section>
       </Body>
