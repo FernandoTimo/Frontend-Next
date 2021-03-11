2919bc3a70c28fbf74e237ccdbfd2755f73857b3
FernandoTimo <fertimo99@gmail.com>
2021-03-11 12:57:58 -0500

    Simple Dash

---
+++ public/assets/dashboard.js
@@ -0,0 +1,16 @@
+// import styleDashboard from 'styles/css/Dashboard.module.css';
+import { Header_Main } from 'heads/Header_Main';
+import { Section, Body, Content } from 'components/Resources/Timoideas';
+export default function Dashboard() {
+  return (
+    <>
+      <Header_Main />
+      Hola
+      {/* <Body>
+        <Section size={1}>
+          <h1>Dashboard</h1>
+        </Section>
+      </Body> */}
+    </>
+  );
+}
