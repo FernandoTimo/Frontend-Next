fc079be163f2d58ddb7b2a72342dfc79199e3389
FernandoTimo <fertimo99@gmail.com>
2021-03-11 17:42:58 -0500

    Styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import style from "styles/css/Index.module.css";
-import { Header_Main } from "heads/Header_Main";
-import { Body, Section, Content } from "components/Resources/Timoideas";
-import BienvenidaSockets from "components/BienvenidaSockets";
+import style from 'styles/css/Index.module.css';
+import { Header_Main } from 'heads/Header_Main';
+import { Body, Section, Content } from 'components/Resources/Timoideas';
+import BienvenidaSockets from 'components/BienvenidaSockets';
 export default function Index() {
   return (
     <>
