3d2ca4eb19f183189991799f00f149977e5ef39d
FernandoTimo <fertimo99@gmail.com>
2021-03-11 12:55:30 -0500

    Solved

---
+++ public/assets/_app.js
@@ -0,0 +1,18 @@
+import '../styles/Global.css';
+import '../styles/Timoideas.css';
+import '../styles/Timoideas.min.css';
+import { ThemeContextProvider } from 'context/ThemeContext';
+import { StoreContextProvider } from 'context/StoreContext';
+import { Navigation } from 'components/Resources/Timoideas';
+const App = ({ Component, pageProps }) => {
+  return (
+    <ThemeContextProvider>
+      <StoreContextProvider>
+        {/* <Navigation /> */}
+        <Component {...pageProps} />
+      </StoreContextProvider>
+    </ThemeContextProvider>
+  );
+};
+
+export default App;
