dc7399c02b4725c8eaae67f154e848a15d569d5a
FernandoTimo <fertimo99@gmail.com>
2021-03-17 11:40:36 -0500

    v0.0.2

--- hooks/useSessionStorage.js
+++ hooks/useSessionStorage.js
@@ -5,20 +5,29 @@ import { useEffect, useState } from 'react';
  * @param {(String|Number|Array)} [value] Valor con el que se actualizará el item
  * @returns {String}
  */
-export default function useSessionStorage(key, value) {
-  const [StorageValue, setStorageValue] = useState();
-  if (!key && key !== '') {
+export default function useSessionStorage(key, value = '') {
+  if (!key || typeof key != 'string' || !!!key.trim()) {
     throw new Error(
       '⚡ useSessionStorage => Proporciona un valor de tipo string como parámetro, ejemplo: useSessionStorage("Theme") ⚡'
     );
   }
+  const [Value, setValue] = useState('');
+  // Esteblece valores de inicio
   useEffect(() => {
-    if (!value) {
-      setStorageValue(window.sessionStorage.getItem(key));
-    } else {
-      window.sessionStorage.setItem(key, value);
-      setStorageValue(window.sessionStorage.getItem(key));
+    // La key no existe en el sessionStorage
+    if (!!!sessionStorage[key]) {
+      sessionStorage[key] = value;
+      setValue(value);
     }
-  }, [key, value]);
-  return StorageValue;
+    // La key existe en el sessionStorage
+    else {
+      setValue(sessionStorage[key]);
+    }
+  }, []);
+  // Cambia el valor del sessionStorage
+  useEffect(() => {
+    sessionStorage[key] = Value;
+  }, [Value]);
+
+  return [Value, setValue];
 }
