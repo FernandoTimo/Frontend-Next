d2d3537e6b47835d6b0f8350722433b78f111690
FernandoTimo <fertimo99@gmail.com>
2021-03-17 20:05:06 -0500

    Default View

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import style from 'styles/css/Index.module.css';
 import { Header_Main } from 'heads/Header_Main';
 import { Body, Section, Content } from 'components/Resources/Timoideas';
-import useCounter from 'hooks/useCounter';
-// import BienvenidaSockets from 'components/BienvenidaSockets';
+import BienvenidaSockets from 'components/BienvenidaSockets';
 
 export default function Index() {
-  let Number = useCounter(554, 0, true);
   return (
     <>
       <Header_Main />
       <Body>
         <Section size={1}>
           <Content center flex={0.5}>
-            <h1>{Number}</h1>
-            {/* <BienvenidaSockets /> */}
+            <h1 className={style.Title}>Frontend</h1>
+            <BienvenidaSockets />
           </Content>
         </Section>
       </Body>
