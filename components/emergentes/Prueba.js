36e98d4eb5369b5ed1c0e02abb800340a6dc7586
FernandoTimo <fertimo99@gmail.com>
2021-03-21 16:29:23 -0500

    Index

--- components/emergentes/Prueba.js
+++
@@ -1,9 +0,0 @@
-import style from './Prueba.module.css';
-function Prueba() {
-  return (
-    <div className={style.Child}>
-      <h1>Child</h1>
-    </div>
-  );
-}
-export default Prueba;
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,17 @@
 import style from 'styles/css/Index.module.css';
 import { Header_Main } from 'heads/Header_Main';
-import {
-  Body,
-  Section,
-  Content,
-  Emergente,
-} from 'components/Resources/Timoideas';
-import Prueba from 'components/emergentes/Prueba';
-import { useState } from 'react';
-// import BienvenidaSockets from 'components/BienvenidaSockets';
+import { Body, Section, Content } from 'components/Resources/Timoideas';
+import BienvenidaSockets from 'components/BienvenidaSockets';
 
 export default function Index() {
-  const [Active, setActive] = useState(false);
-  const toggleActive = () => {
-    setActive(!Active);
-  };
   return (
     <>
       <Header_Main />
       <Body>
         <Section size={1}>
           <Content center flex={0.5}>
-            <button>asds</button>
-            <Emergente
-              child={<Prueba />}
-              position={[1, null, null, 1]}
-              active={[Active, toggleActive, true]}
-            >
-              <div className={style.Emergente} onClick={toggleActive}></div>
-            </Emergente>
-            {/* <h1 className={style.Title}>Frontend</h1>
-            <BienvenidaSockets /> */}
+            <h1 className={style.Title}>Frontend</h1>
+            <BienvenidaSockets />
           </Content>
         </Section>
       </Body>
