31094b97501be24e84a8f4abb8b3c31c5a486911
FernandoTimo <fertimo99@gmail.com>
2021-07-11 07:50:21 -0500

    Main head

--- heads/Main.head.js
+++ heads/Main.head.js
@@ -6,7 +6,7 @@ export default function Head_Main() {
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <meta name='mobile-web-app-capable' content='yes'></meta>
-      <title>Frontend</title>
+      <title>Frontend | Next</title>
       <meta
         name='description'
         content='Template para crear un Frontend usando Next.js.'
