a5465939a0c8ba7c872585e5a87176de8012f031
FernandoTimo <fertimo99@gmail.com>
2021-07-11 07:21:29 -0500

    new og:Image & icon

--- heads/Error.head.js
+++ heads/Error.head.js
@@ -2,11 +2,12 @@ import Head from 'next/head';
 export default function Head_Error() {
   return (
     <Head>
-      <link rel='icon' href='/icons/favicon.ico' />
+      <link rel='icon' href='/icons/favicons/fav_error.ico' />
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       <title>404</title>
       <meta name='description' content='Not URL found' />
+      <meta property='og:image' content='images/og/og_error.png'></meta>
     </Head>
   );
 }
