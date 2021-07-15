f0e447bd94caf1fbf8cceaadcd082f5d8454ab2f
FernandoTimo <fertimo99@gmail.com>
2021-07-15 09:30:37 -0500

    Redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '',
+        destination: '',
+        permanent: true,
+      },
+    ];
+  },
   i18n: {
-    locales: ["es-US", "fr", "nl-NL"],
-    defaultLocale: "es-US",
+    locales: ['es-US', 'fr', 'nl-NL'],
+    defaultLocale: 'es-US',
   },
 };
