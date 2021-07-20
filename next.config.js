d31eea4535a860f076c0af5fbe27a504c969dec6
FernandoTimo <fertimo99@gmail.com>
2021-07-20 14:05:58 -0500

    Redirects infinite loop

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   async redirects() {
     return [
-      {
-        source: '/',
-        destination: '/',
-        permanent: true,
-      },
+      // {
+      //   source: '/',
+      //   destination: '/',
+      //   permanent: true,
+      // },
     ];
   },
   // i18n: {
