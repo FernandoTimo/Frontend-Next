e64baab1f4f8c8bb0b384a895eefedff029b1a5d
FernandoTimo <fertimo99@gmail.com>
2021-08-08 17:48:49 -0500

    Redirect

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   async redirects() {
     return [
-      // {
-      //   source: '/',
-      //   destination: '/',
-      //   permanent: true,
-      // },
+      {
+        source: '/index',
+        destination: '/',
+        permanent: true,
+      },
     ];
   },
   // i18n: {
