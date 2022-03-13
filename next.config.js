40b4b73fa21504030a2bc9c22af7ff6283907df2
FernandoTimo <fertimo99@gmail.com>
2022-03-12 21:40:38 -0500

    SVS support

--- next.config.js
+++ next.config.js
@@ -8,6 +8,13 @@ module.exports = {
       },
     ];
   },
+   webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
   // i18n: {
   //   locales: ['es-US', 'fr', 'nl-NL'],
   //   defaultLocale: 'es-US',
